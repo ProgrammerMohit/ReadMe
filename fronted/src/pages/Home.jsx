c048e3de94d5208ecfae8b92f35dcf498eadab25
Mohit Kumar Prasad <it.20203040@gmail.com>
2023-12-18 01:21:09 +0530

    change the url

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:5555/books")
+      .get("https://read-me-7kt4.vercel.app/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
